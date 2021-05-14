2ba773342c6a6a8cbaa8086d4f37277d365c6c95
MYGguy <mygguy613@gmail.com>
2021-05-14 13:34:17 -0400

    javascript init - listen for key attempt

---
+++ Tile Game/script.js
@@ -0,0 +1,44 @@
+//keypress2 = new KeyboardEvent('keydown', 'KeyW');
+
+/*
+function keypress() {
+    addEventListener.KeyboardEvent('keydown', 119);
+    return Boolean;
+};
+*/
+// numw = 119;
+
+// class keypress {
+//     constructor(key) {
+//         this.key = key;
+//     }
+// }
+
+// new keypress('119');
+
+// '119'.keypress();
+
+
+keys = 0x0011;
+
+function keypress(keys) {
+    if (
+    document.addEventListener('keydown', keys)) {
+        console.log('You have pressed Up');
+    } else {
+        console.log('Nope')
+    };
+    return Boolean;
+};
+
+function message() {
+        if (keypress == true) {
+        console.log('You have pressed Up');
+    } else {
+        console.log('Nope');
+    };
+};
+
+keypress(keys);
+
+message();
