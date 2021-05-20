cc43a13b70b0c863a3548513e6cf7f031dc12e9d
MYGguy <mygguy613@gmail.com>
2021-05-20 05:36:39 -0400

    attempts at eventlisteners again

--- Tile Game/script.js
+++ Tile Game/script.js
@@ -1,12 +1,43 @@
-function keypress(press) {
-    if (press == 119) {
-        console.log('You have pressed Up');
-    } else {
-        console.log('Nope');
-    }
-        return;
+// function wMessage(doubleu) {
+//     if (doubleu == 119) {
+//             console.log('You have pressed Up ' + doubleu);
+//         } else {
+//             console.log('Nope');
+//         }
+//     return;
+// };
+
+// function wDectection(ww) {
+//     if (wPress === ww) {
+//         console.log('You have pressed Up wDectection ' + ww + ' ' + ww); 
+//         } else {
+//             console.log('Nope wDetection ' + ww);
+//         };
+//         return;
+// };
+
+function wPress(w) {
+    document.addEventListener('keydown', wMessage); {
+    // if (wPress === w) {
+    //     console.log('You have pressed Up wDectection ' + w + ' ' + w); 
+    //     } else {
+    //         console.log('Nope wDetection ' + w);
+    //     }
+    };
+    return;
 };
 
-var press = addEventListener('keydown', keypress);
+function wMessage(w) {
+    if (wPress === w) {
+        console.log('You have pressed Up wMessage ' + w + ' ' + w); 
+        } else {
+            console.log('Nope wMessage ' + w);
+        }
+    };
 
-keypress();
+function init() {
+    wPress(119);
+    wMessage(119);
+};
+
+init();
